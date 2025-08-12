5db41746f608f5195b005166249b1773776ffa29
samirkariya <samir.kariya@darshan.ac.in>
2025-08-12 20:00:45 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Hello{
   public static void main(String args[]){
-    System.out.println("Hello World in Java");
+    System.out.println("Hello World in Java test for dependent project");
   }
 }
